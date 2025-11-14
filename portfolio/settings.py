f8b453535aafb990463b156ac52bd86e18685d97
amarchaubey5613 <amarch5613@gmail.com>
2025-11-14 23:10:28 +0530

    Set STATIC_ROOT for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Static files root for collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
